b12d9612748f8b008f30c55b3ed33d3e3989a47d
Sandesh Mankar <mankarsandesh11@gmail.com>
2023-09-12 14:01:26 +0530

    feat : movie autoplay settings

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
 	}, [])
 
 	return (
-		<div className=" flex px-6 pt-1 bg-black from-black items-stretch">
+		<div className="fixed top-0 z-10 w-full flex px-6 pt-1 bg-black from-black items-stretch">
 			<img
 				className="w-24 h-12"
 				src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,7 +9,11 @@ const VideoBackground = ({ movieId }) => {
 		<div className="w-screen">
 			<iframe
 				className="w-screen aspect-video"
-				src={'https://www.youtube.com/embed/' + TrailerVideo?.key}
+				src={
+					'https://www.youtube.com/embed/' +
+					TrailerVideo?.key +
+					'?&autoplay=1&mute=1'
+				}
 				title="YouTube video player"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 			></iframe>
